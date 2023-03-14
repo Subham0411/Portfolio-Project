795aa14f56677c9c819e51cc37a683459515ddd6
subham0411 <66645269+subham0411@users.noreply.github.com>
2023-03-15 02:09:56 +0530

    Update Covid Project.sql

--- Covid Project.sql
+++ Covid Project.sql
@@ -70,7 +70,7 @@ Join PortfolioProject..Covidvaccination$ vac
 where dea.continent is not null 
 order by 2,3
 
----USE CET
+---Use CET to perform Calculation on Partition By in previous query
 With PopvsVac (continent, loaction, date, population, new_vaccination, RollingPeopleVaccinated)
 as
 (
@@ -87,7 +87,7 @@ where dea.continent is not null
 Select *, (RollingPeopleVaccinated/population)*100 as Vaccinatedpercent
 From PopvsVac
 
---TEMP TABLE
+-- Using Temp Table to perform Calculation on Partition By in previous query
 DROP Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
 (
@@ -131,35 +131,3 @@ From PercentPopulationVaccinated
 
 
 
-----------Queries used for Tableau Project
-
---1.
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From PortfolioProject..Coviddeath$
---Where location like '%states%'
-where continent is not null 
---Group By date
-order by 1,2
-
---2 Removing unwanted location data
-Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From PortfolioProject..Coviddeath$
---Where location like '%states%'
-Where continent is null 
-and location not in ('World', 'High income', 'Upper middle income', 'Lower middle income', 'European Union', 'Low income')
-Group by location
-order by TotalDeathCount desc
-
-
---3
-Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..Coviddeath$
-Group by Location, Population
-order by PercentPopulationInfected desc
-
---4
-Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..Coviddeath$
---Where location like '%states%'
-Group by Location, Population, date
-order by PercentPopulationInfected desc
